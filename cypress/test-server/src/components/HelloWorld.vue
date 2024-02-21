579653eb03342d98884fb0f8054b8de1d66aeb16
arrigonif <arrigons94@gmail.com>
2024-02-21 18:30:14 +0100

    fix test show default buttons createDialog

--- cypress/test-server/src/components/HelloWorld.vue
+++ cypress/test-server/src/components/HelloWorld.vue
@@ -93,6 +93,7 @@ export default defineComponent({
           props: { message: 'Hello world!' },
         },
         wrapComponent: true,
+        showDefaultButtons: true,
         dialogOptions: {
           width: '600px',
           persistent: true,
